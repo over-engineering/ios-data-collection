a275a752a27eebb59d098308b1986ff6b0a624f2
schyun <schyun9212@gmail.com>
2019-08-15 22:48:40 +0900

    Initial Commit

---
+++ data-collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  data-collection
+//
+//  Created by Jade on 15/08/2019.
+//  Copyright © 2019 Jade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
